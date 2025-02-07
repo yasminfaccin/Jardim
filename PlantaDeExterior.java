43d57c0a3e5b6536f1ad24b04e995bdfd9152669
Yasmin <153131852+yasminfaccin@users.noreply.github.com>
2025-02-07 17:40:46 -0300

    Update PlantaDeExterior.java

--- PlantaDeExterior.java
+++ PlantaDeExterior.java
@@ -1,7 +1,9 @@
 public class PlantaDeExterior extends Planta {
+    
     public PlantaDeExterior(String nome, String cor, int vezesRegar, int duracaoVida, boolean precisaSol) {
         super(nome, cor, vezesRegar, duracaoVida, precisaSol);
     }
+
 
     @Override
     public String tipoPlanta() {
