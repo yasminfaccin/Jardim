757107203f1d9a390511c03a88c320b2cf97315e
Yasmin <153131852+yasminfaccin@users.noreply.github.com>
2025-02-07 17:39:50 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+       
         Jardim meuJardim = new Jardim();
 
-        // Criando algumas plantas
+        //criando algumas plantas
         Flor rosa = new Flor("Rosa", "Vermelha", 3, 2, true, "Rosa");
         PlantaDeInterior samambaia = new PlantaDeInterior("Samambaia", "Verde", 1, 5, false);
         PlantaDeExterior girassol = new PlantaDeExterior("Girassol", "Amarelo", 2, 1, true);
+        Flor orquidea = new Flor("Orquídea", "Roxa", 1, 4, true, "Orquídea");
+        PlantaDeExterior aloeVera = new PlantaDeExterior("Aloe Vera", "Verde", 1, 5, true);
 
-        // Adicionando plantas ao jardim
+        //plantas no jardim
         meuJardim.adicionarPlanta(rosa);
         meuJardim.adicionarPlanta(samambaia);
         meuJardim.adicionarPlanta(girassol);
+        meuJardim.adicionarPlanta(orquidea);
+        meuJardim.adicionarPlanta(aloeVera);
 
-        // Listando as plantas do jardim
         meuJardim.listarPlantas();
     }
 }
